5aa8125c0cb65dcb27093822a62e322128c750a3
smileybmiley <smileybmiley0521@gmail.com>
2026-02-11 12:33:23 +0900

    Update not sensitive informations

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -6,6 +6,7 @@ import {
   SHARE_ADDRESS_TITLE,
   WEDDING_DATE,
   WEDDING_DATE_FORMAT,
+  KTALK_MSG_TEMPLATE_ID
 } from "../../const"
 import ktalkIcon from "../../icons/ktalk-icon.png"
 import { LazyDiv } from "../lazyDiv"
@@ -24,50 +25,8 @@ export const ShareButton = () => {
             return
           }
 
-          kakao.Share.sendDefault({
-            objectType: "location",
-            address: SHARE_ADDRESS,
-            addressTitle: SHARE_ADDRESS_TITLE,
-            content: {
-              title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
-              description:
-                WEDDING_DATE.format(WEDDING_DATE_FORMAT) + "\n" + LOCATION,
-              imageUrl:
-                window.location.protocol +
-                "//" +
-                window.location.host +
-                baseUrl +
-                "/preview_image.png",
-              link: {
-                mobileWebUrl:
-                  window.location.protocol +
-                  "//" +
-                  window.location.host +
-                  baseUrl,
-                webUrl:
-                  window.location.protocol +
-                  "//" +
-                  window.location.host +
-                  baseUrl,
-              },
-            },
-            buttons: [
-              {
-                title: "초대장 보기",
-                link: {
-                  mobileWebUrl:
-                    window.location.protocol +
-                    "//" +
-                    window.location.host +
-                    baseUrl,
-                  webUrl:
-                    window.location.protocol +
-                    "//" +
-                    window.location.host +
-                    baseUrl,
-                },
-              },
-            ],
+          kakao.Share.sendCustom({
+            templateId: KTALK_MSG_TEMPLATE_ID
           })
         }}
       >
