0e523ebcd7a3c1f19368f99d9ecc50a2dbb7dfcc
smileybmiley <smileybmiley0521@gmail.com>
2026-02-15 14:05:42 +0900

    Remove Location component and associated code
    Removed the Location component and its related imports.

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -39,47 +39,6 @@ export const Location = () => {
           <div className="content">
             네이버 지도, 카카오 네비, 티맵 등 이용
             <br />
-            <b>아레나파크mport { Map } from "./map"
-import CarIcon from "../../icons/car-icon.svg?react"
-import BusIcon from "../../icons/bus-icon.svg?react"
-import { LazyDiv } from "../lazyDiv"
-import { LOCATION, LOCATION_ADDRESS } from "../../const"
-
-export const Location = () => {
-  return (
-    <>
-      <LazyDiv className="card location">
-        <h2 className="english">Location</h2>
-        <div className="addr">
-          {LOCATION}
-          <div className="detail">{LOCATION_ADDRESS}</div>
-        </div>
-        <Map />
-      </LazyDiv>
-      <LazyDiv className="card location">
-        <div className="location-info">
-          <div className="transportation-icon-wrapper">
-            <BusIcon className="transportation-icon" />
-          </div>
-          <div className="heading">대중교통</div>
-          <div />
-          <div className="content">
-            * 지하철 이용시
-            <br />
-            1호선 <b>도원역 1번출구</b> (도보 5분 이내) 또는
-            <br />
-            도원역 지하 연결통로 직진.
-          </div>
-        </div>
-        <div className="location-info">
-          <div className="transportation-icon-wrapper">
-            <CarIcon className="transportation-icon" />
-          </div>
-          <div className="heading">자가용</div>
-          <div />
-          <div className="content">
-            네이버 지도, 카카오 네비, 티맵 등 이용
-            <br />
             <b>아레나파크 웨딩홀</b> 검색
             <br />
             - 주차 요금은 무료입니다.
