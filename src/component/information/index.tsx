2b92df0fcd07129284eefbf7750f48a23d00222c
smileybmiley <smileybmiley0521@gmail.com>
2026-02-15 13:58:40 +0900

    Update meal time and location information

--- src/component/information/index.tsx
+++ src/component/information/index.tsx
@@ -12,9 +12,9 @@ export const Information1 = () => {
       <div className="info-card">
         <div className="label">식사 안내</div>
         <div className="content">
-          식사시간: 12시 30분 ~ 14시 30분
+          식사시간: 12시 50분 ~ 14시 50분
           <br />
-          장소: 지하 1층 연회장
+          장소: 1층 연회장 (식장과 같은 층입니다)
         </div>
       </div>
     </>
